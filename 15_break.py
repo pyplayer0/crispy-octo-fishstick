71ef811bdd78b0c458e72b59abe1426100069463
fish add <rkswlpy@gmail.com>
2024-03-28 11:10:08 +0900

    continue예제 추가

--- 15_break.py
+++ 15_break.py
@@ -2,7 +2,12 @@ index = ["시즌1", "시즌2", "시즌3", "시즌4", "시즌5"]
 for x in index:
     if x=="시즌3":
         print("여주가 바꼈다고? 어~ 안봐요")
-        
         break
     print(f"{x} 시청")
     
+# continue
+for x in index:
+    if x=="시즌3":
+        print("여주가 바꼈다고? 어~ 안봐요")
+        continue
+    print(f"{x} 시청")
