85f742fc118ef54d861ce346d5a0fba49d99af57
fish add <rkswlpy@gmail.com>
2024-03-28 10:47:26 +0900

    if문 예제

--- 12_if문.py
+++ 12_if문.py
@@ -1,1 +1,19 @@
-1
+# 조건이 True일 경우 if문내의 코드를 실행
+
+total = 2 
+if total >= 4:
+    print("추가비용 없음")
+else:
+    print("1인당 만원")
+print("감사합니다") # 추가비용 없음 감사합니다 
+
+yellow_card = 0
+foul = True
+if foul:
+    yellow_card += 1
+    if yellow_card == 2:
+        print("퇴장")
+    else:
+        print("옐로 카드 누적 1장")
+else:
+    print("주의")
